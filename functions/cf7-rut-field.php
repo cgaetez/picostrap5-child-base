566556c9fd1d83c9e6112d30a79c4158d44a98ea
cgaetez <claudiagaetez@gmail.com>
2025-08-29 18:07:52 +0200

    acf form

--- functions/cf7-rut-field.php
+++ functions/cf7-rut-field.php
@@ -1,68 +1,98 @@
 <?php
+  // 1. Registrar los tags personalizados
+add_action('wpcf7_init', function () {
   wpcf7_add_form_tag('rut', 'rut_not_required_handler', true);
   wpcf7_add_form_tag('rut*', 'rut_required_handler', true);
+});
 
-  function rut_not_required_handler($tag) { return rut_handler($tag, false); }
-  function rut_required_handler($tag) { return rut_handler($tag, true); }
+// 2. Render del campo RUT (obligatorio o no)
+function rut_not_required_handler($tag) {
+  return rut_handler($tag, false);
+}
 
-  function rut_handler($tag, $required) {
-    $name = 'rut';
-    $id = 'rut';
-    $class = 'wpcf7-form-control wpcf7-text';
-    $placeholder = '';
+function rut_required_handler($tag) {
+  return rut_handler($tag, true);
+}
 
-    if($required) {
-      $class .= ' wpcf7-validates-as-required';
+function rut_handler($tag, $required) {
+  $name = $tag['name'];
+  $value = isset($_POST[$name]) ? esc_attr($_POST[$name]) : '';
+  $id = 'rut';
+  $class = 'wpcf7-form-control wpcf7-text';
+  $placeholder = '';
+
+  if ($required) {
+    $class .= ' wpcf7-validates-as-required';
+  }
+
+  foreach ($tag['options'] as $option) {
+    $parts = explode(':', $option, 2);
+    if ($parts[0] === 'id') {
+      $id = esc_attr($parts[1]);
+    } elseif ($parts[0] === 'class') {
+      $class .= ' ' . esc_attr($parts[1]);
+    } elseif ($parts[0] === 'placeholder') {
+      $placeholder = esc_attr($parts[1]);
     }
-
-    if (!empty($tag['name']))
-      $name = $tag['name'];
-
-    for ($i = 0; $i < count($tag['options']); $i++) {
-      $option = explode(':', $tag['options'][$i]);
-
-      if ($option[0] == 'id') {
-        $id = $option[1];
-      } else if ($option[0] == 'class') {
-        $class .= " $option[1]";
-      } else if ($option[0] == 'placeholder') {
-        $placeholder = $option[1];
-      }
-    }
-
-    $nameTag = "name=\"$name\"";
-    $idTag = "id=\"$id\"";
-    $classTag = empty($class) ? '' : "class=\"$class\"";
-    $placeholderTag = empty($placeholder) ? '' : "placeholder=\"$placeholder\"";
-    $sizeTag = "size=\"40\"";
-    $ariaRequiredTag = $required ? "aria-required=\"true\"" : "aria-required=\"false\"";
-    $ariaInvalidTag = "aria-invalid=\"false\"";
-
-    return "
-      <span class=\"wpcf7-form-control-wrap\" data-name=\"$name\">
-        <input type=\"text\" $nameTag $idTag $placeholderTag $classTag $sizeTag $ariaRequiredTag $ariaInvalidTag value=\"\" />
-      </span>";
   }
 
-  add_filter('wpcf7_validate_rut', 'wpcf7_validate_rut_not_required_handler', 20, 2);
-  add_filter('wpcf7_validate_rut*', 'wpcf7_validate_rut_required_handler', 20, 2);
+  return sprintf(
+    '<span class="wpcf7-form-control-wrap" data-name="%s">
+      <input type="text" name="%s" id="%s" value="%s" class="%s" placeholder="%s" size="40" aria-required="%s" aria-invalid="false" />
+    </span>',
+    esc_attr($name),
+    esc_attr($name),
+    esc_attr($id),
+    $value,
+    esc_attr($class),
+    $placeholder,
+    $required ? 'true' : 'false'
+  );
+}
 
-  function wpcf7_validate_rut_not_required_handler($result, $tag) {
-    return wpcf7_validate_rut_handler($result, $tag, false);
+// 3. Validación del campo RUT
+add_filter('wpcf7_validate_rut', 'wpcf7_validate_rut_not_required_handler', 20, 2);
+add_filter('wpcf7_validate_rut*', 'wpcf7_validate_rut_required_handler', 20, 2);
+
+function wpcf7_validate_rut_not_required_handler($result, $tag) {
+  return wpcf7_validate_rut_handler($result, $tag, false);
+}
+
+function wpcf7_validate_rut_required_handler($result, $tag) {
+  return wpcf7_validate_rut_handler($result, $tag, true);
+}
+
+function wpcf7_validate_rut_handler($result, $tag, $required) {
+  $name = $tag->name;
+  $value = isset($_POST[$name]) ? $_POST[$name] : '';
+
+  if ($required && empty($value)) {
+    $result->invalidate($tag, 'El campo es obligatorio.');
+  } elseif (!empty($value) && !is_valid_rut($value)) {
+    $result->invalidate($tag, 'El RUT ingresado no es válido.');
   }
 
-  function wpcf7_validate_rut_required_handler($result, $tag) {
-    return wpcf7_validate_rut_handler($result, $tag, true);
+  return $result;
+}
+
+// 4. Validador de RUT chileno (Módulo 11)
+function is_valid_rut($rut) {
+  $rut = preg_replace('/[^0-9kK]/', '', $rut);
+  if (strlen($rut) < 2) return false;
+
+  $body = substr($rut, 0, -1);
+  $dv = strtoupper(substr($rut, -1));
+  $suma = 0;
+  $multiplo = 2;
+
+  for ($i = strlen($body) - 1; $i >= 0; $i--) {
+    $suma += intval($body[$i]) * $multiplo;
+    $multiplo = $multiplo < 7 ? $multiplo + 1 : 2;
   }
 
-  function wpcf7_validate_rut_handler($result, $tag, $required) {
-    $value = $_POST[$tag->name];
+  $resto = $suma % 11;
+  $dvEsperado = 11 - $resto;
+  $dvEsperado = $dvEsperado == 11 ? '0' : ($dvEsperado == 10 ? 'K' : (string)$dvEsperado);
 
-    if ($required && empty($value)) {
-      $result->invalidate($tag, 'El campo es obligatorio.');
-    } elseif (!empty($value) && !is_valid_rut($value)) {
-      $result->invalidate($tag, 'El RUT indicado no es válido.');
-    }
-
-    return $result;
-  }
+  return $dv === $dvEsperado;
+}
